adb4b082bc73a14cec4c47326b722e60d319bd29
EbaboTV <161514662+EbaboTV@users.noreply.github.com>
2025-09-08 06:12:03 +0000

    lab1

---
+++ sites/skuit/www/lab1/hello_php.php
@@ -0,0 +1,11 @@
+<?php
+$sum = 2006 + 25 -1;
+$fio = 'Эрик ' . 'Павлов'
+$length = 1 * 2;
+$width = 11 + 7;
+$square = $length  * $width;
+
+echo "Результат сложения трёх чисел: " . $sum . "\n";
+echo "Меня зовут: " . $fio . "\n";
+echo "Площадь: " . $square . "\n";
+?>
